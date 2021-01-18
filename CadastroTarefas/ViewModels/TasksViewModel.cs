7fb66bda64dd68e10e02f2f92fd7c343aecad5cc
Vinícius Luiz Lopes Nunes <viniciuslln@outlook.com>
2021-01-18 10:15:08 -0300

    Implementado adição de nova tarefa

--- CadastroTarefas/ViewModels/TasksViewModel.cs
+++ CadastroTarefas/ViewModels/TasksViewModel.cs
@@ -2,12 +2,13 @@
 using MvvmHelpers;
 using MvvmHelpers.Commands;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CadastroTarefas.ViewModels
 {
-    public class TasksViewModel: BaseViewModel
+    public class TasksViewModel : BaseViewModel
     {
         private string newTaskDescription;
 
@@ -17,19 +18,61 @@ namespace CadastroTarefas.ViewModels
             set { SetProperty(ref newTaskDescription, value); }
         }
 
-        public ObservableRangeCollection<object> TodoTasks { get; set; } = new ObservableRangeCollection<object>();
+        public ObservableRangeCollection<UserTask> TodoTasks { get; set; } = new ObservableRangeCollection<UserTask>();
+        public ObservableRangeCollection<UserTask> CompletedTasks { get; set; } = new ObservableRangeCollection<UserTask>();
 
         public ICommand SaveTaskCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand CompleteCommand { get; set; }
+        public ICommand RemoveCommand { get; set; }
+
         public TasksViewModel()
         {
-            TodoTasks.Add(new object());
-            TodoTasks.Add(new object());
-            TodoTasks.Add(new object());
-            TodoTasks.Add(new object());
-            TodoTasks.Add(new object());
-            TodoTasks.Add(new object());
-
+            LoadTasks().SafeFireAndForget();
             SaveTaskCommand = new AsyncCommand(SaveTask);
+            EditCommand = new AsyncCommand<UserTask>(EditTask);
+            CompleteCommand = new AsyncCommand<UserTask>(CompleteTask);
+            RemoveCommand = new AsyncCommand<UserTask>(RemoveTask);
+        }
+
+        private async Task RemoveTask(UserTask arg)
+        {
+            using (var db = new CadastroTarefasContext())
+            {
+                db.UserTasks.Remove(arg);
+                await LoadTasks();
+            }
+        }
+
+        private async Task CompleteTask(UserTask arg)
+        {
+            using (var db = new CadastroTarefasContext())
+            {
+                arg.TaskStatus = DataLayer.TaskStatus.COMPLETED;
+                db.UserTasks.Update(arg);
+                await LoadTasks();
+            }
+        }
+
+        private async Task EditTask(UserTask arg)
+        {
+
+
+
+        }
+
+        private async Task LoadTasks()
+        {
+            using (var db = new CadastroTarefasContext())
+            {
+                TodoTasks.Clear();
+                CompletedTasks.Clear();
+
+                db.UserTasks.Where(ut => ut.UserId == 1 && ut.TaskStatus == DataLayer.TaskStatus.TODO).ToList()
+                    .ForEach(t => TodoTasks.Add(t));
+                db.UserTasks.Where(ut => ut.UserId == 1 && ut.TaskStatus == DataLayer.TaskStatus.COMPLETED).ToList()
+                    .ForEach(t => CompletedTasks.Add(t));
+            }
         }
 
         private async Task SaveTask()
@@ -39,6 +82,8 @@ namespace CadastroTarefas.ViewModels
                 var task = new UserTask { Description = NewTaskDescription, UserId = 1, TaskStatus = DataLayer.TaskStatus.TODO };
                 await db.UserTasks.AddAsync(task);
                 await db.SaveChangesAsync();
+                NewTaskDescription = "";
+                await LoadTasks();
             }
         }
     }
